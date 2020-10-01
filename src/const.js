cd8ffd3d984f19d49935153de648d11fa2f96ab2
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-10-02 01:08:07 +0400

    implements authorization in spotify api

--- src/const.js
+++ src/const.js
@@ -1,5 +1,6 @@
 const SETTINGS = {
   CLIENT_ID: `d80df39bde594e5d9e7e78e2038bdfb1`,
+  APP_URL: `http://localhost:8080/`,
 };
 
 const RenderPosition = {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
+import {SETTINGS} from './const';
 import {render} from './util/render';
 import AuthorizationView from './views/authorization';
 
+const generateAuthorizationLink = () => {
+  return `https://accounts.spotify.com/authorize?client_id=${SETTINGS.CLIENT_ID}&redirect_uri=${SETTINGS.APP_URL}&scope=user-read-private%20user-read-email&response_type=token&state=123`;
+};
+
 const siteMainElement = document.querySelector(`.main`);
 
-render(siteMainElement, new AuthorizationView(`#`));
+const authorizationLink = generateAuthorizationLink();
+
+const hash = window.location.hash;
+
+switch (true) {
+  case (hash.includes(`access_token`)):
+    document.querySelector(`body`).textContent = `SUCCESS`;
+    break;
+  case (hash.includes(`error`)):
+    document.querySelector(`body`).textContent = `ERROR`;
+    break;
+  default:
+    render(siteMainElement, new AuthorizationView(authorizationLink));
+    break;
+}
