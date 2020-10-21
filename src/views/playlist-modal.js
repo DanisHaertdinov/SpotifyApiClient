5d3a48e2b326878f3e1d0812a6902a0bd8cb5277
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-10-22 00:21:28 +0400

    styling playlist-details component

--- src/views/playlist-modal.js
+++ src/views/playlist-modal.js
@@ -5,7 +5,7 @@ const createPlaylistModalTracksTemplate = (tracks) => {
     const {title, album, duration} = track;
 
     return (
-      `<tr>
+      `<tr class="playlist-details__track track">
         <td>${title}</td>
         <td>${album}</td>
         <td>${duration}</td>
@@ -16,23 +16,30 @@ const createPlaylistModalTracksTemplate = (tracks) => {
 };
 
 const createPlaylistModalTemplate = (playlist, tracks) => {
-  const {imageUrl, title, description, tracksCount, author} = playlist;
+  const {imageUrl, title, description, tracksCount, owner} = playlist;
+  const {author} = owner;
   return (
     `<div class="playlist-details">
-      <img class="playlist-details__image" alt="" src="${imageUrl}">
-      <h3 class="playlist-details__title">${title}</h3>
-      <p class="playlist-details__description">${description}</p>
-      <p class="playlist-details__author">${author}</p>
-      <p class="playlist-details__tracks-count">${tracksCount} tracks</p>
-      <table>
-        <tr>
-          <th>title</th>
-          <th>album<th>
-          <th>duration</th>
-          <th></th>
-        </tr>
-        ${createPlaylistModalTracksTemplate(tracks)}
-      </table>
+      <div class="playlist-details__header">
+        <img class="playlist-details__image" alt="" src="${imageUrl}" width="324" height="324">
+        <div class="playlist-details__info">
+          <h3 class="playlist-details__title">${title}</h3>
+          <p class="playlist-details__description">${description}</p>
+          <p class="playlist-details__author">${author}</p>
+          <p class="playlist-details__tracks-count">${tracksCount} tracks</p>
+        </div>
+      </div>
+      <div class="playlist-details__body">
+        <table class="playlist-details__tracks">
+          <tr>
+            <th>Title</th>
+            <th>Album</th>
+            <th>Duration</th>
+            <th></th>
+          </tr>
+          ${createPlaylistModalTracksTemplate(tracks)}
+        </table>
+      </div>
   </div>`
   );
 };
