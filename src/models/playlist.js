e80e65554f771b5a04257c53b0da179b471f0cea
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-10-21 08:45:57 +0400

    refactor playlist modal adapter method

--- src/models/playlist.js
+++ src/models/playlist.js
@@ -2,11 +2,11 @@ export default class Playlist {
 
   static adaptToClient(playlist) {
     return {
-      id: playlist.id,
-      description: playlist.description,
-      imageUrl: playlist.images[0].url,
-      title: playlist.name,
-      tracksCount: playlist.tracks.total,
+      id: playlist[`id`],
+      description: playlist[`description`],
+      imageUrl: playlist[`images`][0][`url`],
+      title: playlist[`name`],
+      tracksCount: playlist[`tracks`][`total`],
     };
   }
 }
